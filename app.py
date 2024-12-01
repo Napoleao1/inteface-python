3a03ff06e3d793ae9fd8ea38e0e52f5228741c8c
Napoleao1 <ernaninandoxd@gmail.com>
2024-12-01 20:39:16 -0300

    app interface

---
+++ app.py
@@ -0,0 +1,15 @@
+import customtkinter as ctk
+
+#configurando aparência
+ctk.set_appearance_mode('dark')
+
+#criação da janela principal
+app = ctk.CTk()
+app.title('Sistema de Login')
+app.geometry('300x300')
+
+
+
+
+
+#Iniciar Aplicação
